66bed8c5fb2f5272f28dec4a7cbd7ca8b4c4e8c8
Blair Vanderhoof <blairv@blairv-mbp.dhcp.thefacebook.com>
2014-11-11 01:07:35 -0800

    Initial Commit

---
+++ StickerCam/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  StickerCam
+//
+//  Created by Blair Vanderhoof on 11/11/14.
+//  Copyright (c) 2014 blairv. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
